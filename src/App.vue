bc56f23c6b54db1d7d1512f264efdd988b3cb9de
Charlilyyyy <mohamadtaufik099@gmail.com>
2023-11-13 00:22:10 +0800

    done set balance and transaction history

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,34 @@
-<template>Expense Tracker App</template>
+<template>
+    <Header/>
+    <div class="container">
+        <Balance :total="total"/>
+        <IncomeExpenses/>
+        <TransactionList :transactions="mainTransactions"/>
+        <AddTransaction/>
+    </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+import { ref,computed } from 'vue';
+
+const mainTransactions = ref([
+    { id: 1, text: 'Flower', amount: -19.99 },
+    { id: 2, text: 'Salary', amount: 299.97 },
+    { id: 3, text: 'Book', amount: -10.00 },
+    { id: 4, text: 'Camera', amount: 150.00 }
+])
+
+const total = computed(()=>{
+    return mainTransactions.value.reduce((accumulator,currTransaction)=>{
+        return accumulator + currTransaction.amount
+    },0)
+})
+
+
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>History</h3>
+      <ul id="list" class="list">
+        <!-- <li class="minus">
+          Cash <span>-MYR400</span><button class="delete-btn">x</button>
+        </li>
+        <li class="plus">
+          Paycheck <span>+MYR800</span><button class="delete-btn">x</button>
+        </li> -->
+        <li
+            v-for="transaction in transactions"
+            :key="transaction.id"
+            :class="transaction.amount > 0 ? 'plus' : 'minus'"
+        >
+            {{ transaction.text }} <span>MYR{{ transaction.amount }}</span><button class="delete-btn">x</button>
+        </li>
+      </ul>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            required: true,
+        }
+    })
+</script>
